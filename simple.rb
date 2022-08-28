989b8de57ad6fc4f922dc6f9af770f2c314a4e4f
deepak511 <deepak.prasad@coindcx.com>
2022-08-28 14:22:06 +0530

    add simple.rb

---
+++ simple.rb
@@ -0,0 +1,26 @@
+#!/usr/bin/env ruby
+
+
+
+def find_v(word=[])
+    v=['a','e','i','o','u']
+    word.find_index{|ch| v.include?(ch)}
+end
+
+def pig_latin(word="")
+    word.downcase!
+    char=word.split(//)
+    first=find_v(char)
+    
+    if first>0
+        front = char[0...first]
+        back =char[first..-1]
+    else
+        front=[]
+        back=char
+    end
+
+    back.join + front.join + "ay"
+end
+
+puts pig_latin("deepak")
